7acbeb1b7594c62f7c0611961e0cad7d341ece0c
Facundo Petre <fpetre@integraccs.com>
2018-09-23 01:38:28 -0300

    gh-pages

--- mooveit/src/TicTacToe/components/Line.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Square from './Square'
-const Line = props => (
-  props.SquaresList.map((elem, index) =><Square onClick={props.onClick} key={index}>{elem[0]}</Square>)
-)
-
-
-export default Line
--- mooveit/src/TicTacToe/components/Square.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './square.css'
-
-const Square = props => (
-  <button className="Square" onClick={props.onClick}>{props.children}</button>
-)
-
-export default Square
