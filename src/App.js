604f5447402e36f27865ed97b4e53bc6dc140501
Facundo Petre <fpetre@integraccs.com>
2018-09-23 12:14:07 -0300

    Remove unused files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import SayHi from './custom-component/component/cutom-component'
 import TicTacToe from './TicTacToe/containers/Board-container'
 class App extends Component {
   state = {
